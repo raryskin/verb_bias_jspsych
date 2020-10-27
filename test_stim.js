def4c4bb42da088f5de2dc33bd09109406ed27b6
raryskin <rryskin@gmail.com>
2020-10-27 15:28:03 -0700

    working version with appropriate sounds

---
+++ test_stim.js
@@ -0,0 +1,34 @@
+var test_stim = [
+{
+  List: 2,
+  TestOrder: 80,
+  TrialID: 3,
+  Type: "mod",
+  Verb: "squeeze",
+  TargetAnimal: "bunny",
+  TargetInstrument: "microphone",
+  DistractorAnimal: "pig",
+  DistractorInstrument: "spoon",
+  "ta": "bunny_microphone",
+  "ti": "microphone",
+  "da": "pig_spoon",
+  "di": "spoon",
+  soundID: "56"
+},
+{
+  List: 2,
+  TestOrder: 81,
+  TrialID: 26,
+  Type: "mod",
+  Verb: "cuddle",
+  TargetAnimal: "whale",
+  TargetInstrument: "microphone",
+  DistractorAnimal: "turtle",
+  DistractorInstrument: "flower",
+  "ta": "whale_microphone",
+  "ti": "microphone",
+  "da": "turtle_flower",
+  "di": "flower",
+  soundID: "84"
+}
+]
